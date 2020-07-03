7637db39667d785a288fdda9b36c54ee9f6a0658
amulya reddy <amulyareddy@amulyas-MacBook-Air.local>
2020-07-02 22:07:34 -0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Spinner from './Components/Spinner';
 import Table from './Components/Table';
+import Alert from './Components/Alert';
 import './App.css';
 import { compareValues } from './utils';
 
@@ -8,15 +9,24 @@ function App() {
   const [search, updateSearchText] = useState('');
   const [loading, updateLoading] = useState(false);
   const [books, updateBooks] = useState([]);
+  const [error, updateErrorState] = useState(false);
 
   const updateSearch = async () => {
-    updateLoading(true);
-    const res = await fetch(
-      `http://openlibrary.org/search.json?title=${search}`
-    );
-    const books = await res.json();
-    updateLoading(false);
-    updateBooks(books.docs.sort(compareValues('title')).slice(0, 20));
+    try {
+      updateLoading(true);
+      const res = await fetch(
+        `http://openlibrary.org/search.json?title=${search}`
+      );
+      const books = await res.json();
+      updateBooks(books.docs.sort(compareValues('title')).slice(0, 20));
+      updateErrorState(false);
+    } catch (err) {
+      console.error(error);
+      updateErrorState(true);
+      updateBooks([]);
+    } finally {
+      updateLoading(false);
+    }
   };
   return (
     <div>
@@ -29,12 +39,14 @@ function App() {
             type="text"
             className="form-control"
             placeholder="Title of the book (Ex: The lord of rings)"
+            disabled={loading}
             onChange={(event) => updateSearchText(event.target.value)}
             aria-label="Amount (to the nearest dollar)"
           />
           <div className="input-group-append">
             <span
               className="input-group-text cursor-pointer"
+              disabled={loading}
               onClick={updateSearch}
             >
               Search
@@ -46,7 +58,8 @@ function App() {
             <Spinner />
           </div>
         )}
-        <div className="mt-5">{!loading && <Table books={books} />}</div>
+        <div className="mt-5">{!loading && !error && <Table books={books} />}</div>
+        <div className="mt-5">{!loading && error && <Alert />}</div>
       </div>
     </div>
   );
